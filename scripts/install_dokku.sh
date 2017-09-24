6fe42daafef471dc993b5109f31fd7bafd9617fe
Edward Viaene <ward.viaene@gmail.com>
2017-09-24 11:07:51 +0200

    version bump

--- scripts/install_dokku.sh
+++ scripts/install_dokku.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sudo apt-get update
-wget https://raw.githubusercontent.com/dokku/dokku/v0.5.6/bootstrap.sh
-sudo DOKKU_TAG=v0.5.6 bash bootstrap.sh
+wget https://raw.githubusercontent.com/dokku/dokku/v0.10.4/bootstrap.sh
+sudo DOKKU_TAG=v0.10.4 bash bootstrap.sh
 dokku apps:create dokku-demo
 dokku plugin:install https://github.com/dokku/dokku-mongo.git mongo
 dokku mongo:create dokku-demo-db
